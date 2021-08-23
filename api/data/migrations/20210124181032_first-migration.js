2b40be4a39b78a13953d35e03a82d6eb8e58f905
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-23 14:02:35 -0500

    item server and migration work

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -7,8 +7,23 @@ exports.up = async (knex) => {
       users.boolean('owner').notNullable()
       users.timestamps(false, true)
     })
+    .createTable('items', (items) =>{
+      items.increments('items_id')
+      items.integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('user_id')
+      .inTable('users')
+      .onDelete('RESTRICT')
+      .onUpdate('RESTRICT')
+      items.string('item_name', 30).notNullable()
+      items.string('item_description', 50).notNullable()
+      items.float('item_price').notNullable()
+      items.string('item_country').notNullable()
+    })
 }
 
 exports.down = async (knex) => {
   await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists('items')
 }
--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const authRouter = require("./auth/auth-router");
 const usersRouter = require("./users/users-router");
+const itemsRouter = require("./items/items-router")
 
 const server = express()
 
@@ -13,9 +14,9 @@ server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter); 
+server.use('/api/users', usersRouter);
+server.use('/api/items', itemsRouter);
 
 server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
