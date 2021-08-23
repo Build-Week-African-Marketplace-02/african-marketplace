037413730b6738e4a04e2372d6ecfc81bdeb255b
adampatt <adampattison08@gmail.com>
2021-08-23 15:43:13 +0200

    users getAll and insertUser working

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,12 +2,13 @@ const router = require("express").Router();
 const Users = require("./users-model")
 
 
-router.get("/api/users", async (req, res) => {
-  res.json(await getAllUsers());
+router.get("/", async (req, res) => {
+  res.json(await Users.getAllUsers());
+  // res.json("users get api")
 });
 
-router.post("/api/users", async (req, res) => {
-  res.status(201).json(await insertUser(req.body));
+router.post("/", async (req, res) => {
+  res.status(201).json(await Users.insertUser(req.body));
 });
 
 
