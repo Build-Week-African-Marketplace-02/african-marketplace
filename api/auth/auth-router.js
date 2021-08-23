e9e9e015ee05fb155013f83db673c6d942c502bc
adampatt <adampattison08@gmail.com>
2021-08-23 16:32:14 +0200

    login route working with test data

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 
 const router = require("express").Router();
 
-// const tokenBuilder = require("./auth-token-builder");
+const tokenBuilder = require("./auth-token-builder");
 const Users = require("../users/users-model");
 
 router.post("/register", (req, res, next) => {
@@ -15,5 +15,24 @@ router.post("/register", (req, res, next) => {
       .catch(next);
   }
 );
+
+
+router.post("/login", (req, res, next) => {
+  // const { username, password } = req.body;
+  // Users.findBy({ username }) 
+  //   .then(([user]) => {
+  //     if (user && bcrypt.compareSync(password, user.password)) {
+  //       const token = tokenBuilder(user);
+  //       res.status(200).json({
+  //         message: `Welcome back ${user.username}!`,
+  //         token,
+  //       });
+  //     } else {
+  //       next({ status: 401, message: "Invalid Credentials" });
+  //     }
+  //   })
+  //   .catch(next);
+  res.json("login route ")
+});
 
 module.exports = router;
