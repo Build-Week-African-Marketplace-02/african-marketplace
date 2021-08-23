f267aece86f19b0dac6f81cae4002b09f9febb2f
adampatt <adampattison08@gmail.com>
2021-08-23 15:53:23 +0200

    before rollback

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,7 +8,7 @@ const Users = require("../users/users-model");
 router.post("/register", (req, res, next) => {
     const { username, password, owner } = req.body;
     const hash = bcrypt.hashSync(password, 8);
-    Users.insert({ username, password: hash, owner })
+    Users.insertUser({ username, password: hash, owner })
       .then((newUser) => {
         res.status(201).json(newUser);
       })
