fd7ac7560a80d0c54115779a7ab83801a7050970
adampatt <adampattison08@gmail.com>
2021-08-23 17:14:45 +0200

    user can login locally and returns a token

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 const tokenBuilder = require("./auth-token-builder");
 const Users = require("../users/users-model");
 
+
 router.post("/register", (req, res, next) => {
     const { username, password, owner } = req.body;
     const hash = bcrypt.hashSync(password, 8);
@@ -18,21 +19,20 @@ router.post("/register", (req, res, next) => {
 
 
 router.post("/login", (req, res, next) => {
-  // const { username, password } = req.body;
-  // Users.findBy({ username }) 
-  //   .then(([user]) => {
-  //     if (user && bcrypt.compareSync(password, user.password)) {
-  //       const token = tokenBuilder(user);
-  //       res.status(200).json({
-  //         message: `Welcome back ${user.username}!`,
-  //         token,
-  //       });
-  //     } else {
-  //       next({ status: 401, message: "Invalid Credentials" });
-  //     }
-  //   })
-  //   .catch(next);
-  res.json("login route ")
+  const { username, password } = req.body;
+  Users.findBy({ username }) 
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenBuilder(user);
+        res.status(200).json({
+          message: `Welcome back ${user.username}!`,
+          token,
+        });
+      } else {
+        next({ status: 401, message: "Invalid Credentials" });
+      }
+    })
+    .catch(next);
 });
 
 module.exports = router;
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -16,8 +16,12 @@ async function insertUser(user) {
   return newUserObject; // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
 }
 
+ function findBy(filter) {
+  return db("users").select("*").where(filter);
+}
 
 module.exports = {
-    getAllUsers,
-    insertUser
-}
+  getAllUsers,
+  insertUser,
+  findBy,
+};
